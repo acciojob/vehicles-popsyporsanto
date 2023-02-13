7c1da596efe186deb397332671ba49b774448d0a
popsy porsanto <popsyporsanto>
2023-02-13 22:23:18 +0530

    solution 3

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,17 +3,37 @@ package com.driver;
 public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
-    Boat(String name,int capacity){
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    Boat(String name, int capacity){
         this.name = name;
         this.capacity = capacity;
     }
     @Override
     public String getVehicleName() {
+
         return name;
     }
 
     @Override
     public int getVehicleCapacity() {
+
         return capacity;
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,53 +4,40 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name,4,4,6,isManual,"F1",5);
+        super(name, 4, 4, 5, isManual, "luxury", 6);
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
+    public void accelerate(int rate) {
+        int newSpeed = getCurrentSpeed() + rate;
+        ; //set the value of new speed by using currentSpeed and rate
+        /*
          * speed 0: gear 1
          * speed 1-50: gear 1
-         * speed 51-100: gear 2
+         * speed 51-1 gear 2
          * speed 101-150: gear 3
          * speed 151-200: gear 4
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if (newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
-            setCurrentGear(1);
-        }
+            changeGear(1);
+        } else if (newSpeed <= 50) {
+            changeGear(1);
+        } else if (newSpeed <= 100) {
+            changeGear(2);
+        } else if (newSpeed <= 150) {
+            changeGear(3);
+        } else if (newSpeed <= 200) {
+            changeGear(4);
+        } else if (newSpeed <= 250) {
+            changeGear(5);
+        } else changeGear(6);
         //for all other cases, change the gear accordingly
-        else if(newSpeed>=1 && newSpeed<=50){
-            setCurrentGear(1);
-            setCurrentSpeed(newSpeed);
-        }
-        else if(newSpeed>=51 && newSpeed<=100){
-            setCurrentGear(2);
-            setCurrentSpeed(newSpeed);
-        }
-        else if(newSpeed>=101 && newSpeed<=150){
-            setCurrentGear(3);
-            setCurrentSpeed(newSpeed);
-        }
-        else if(newSpeed>=151 && newSpeed<=200){
-            setCurrentGear(4);
-            setCurrentSpeed(newSpeed);
-        }
-        else if(newSpeed>=201 && newSpeed<=250){
-            setCurrentGear(5);
-            setCurrentSpeed(newSpeed);
-        }
-        else if(newSpeed>250) {
-            setCurrentGear(6);
-            setCurrentSpeed(newSpeed);
-        }
 
-        if(newSpeed > 0) {
+        if (newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
     }
